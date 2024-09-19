dc0b8c9c5d71666ce7a41d023528179e726834f6
Bruno Lira <brunolira1@icloud.com>
2024-09-19 12:41:32 -0300

    Implemented decorators

---
+++ src/decorators/param-id-decorator.ts
@@ -0,0 +1,5 @@
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+
+export const ParamId = () => createParamDecorator((_data: unknown, context: ExecutionContext) => {
+    return context.switchToHttp().getRequest().params.id
+})
